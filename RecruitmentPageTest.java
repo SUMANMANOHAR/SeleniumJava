3504255051abc9dc3779082da7cb1e25147bfaaf
SUMANMANOHAR <111900541+SUMANMANOHAR@users.noreply.github.com>
2023-01-17 14:53:17 +0530

    Add files via upload

---
+++ RecruitmentPageTest.java
@@ -0,0 +1,45 @@
+package project2;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import test1.HomePage;
+import test1.MyInfoPage;
+import test1.RecruitmentPage;
+
+public class RecruitmentPageTest {
+
+	WebDriver driver;
+
+	@Test
+	public void f() throws Exception {
+		RecruitmentPage recruitmentpage = new RecruitmentPage(driver);
+		recruitmentpage.addVacancies();
+
+	}
+
+	@BeforeMethod
+	public void launchUrl() throws Exception {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		HomePage homepagetest = new HomePage(driver);
+		homepagetest.checkUsername();
+
+	
+	
+}
+	@AfterMethod
+	public void closeBrowser() {
+		// driver.close();
+	}
+}
